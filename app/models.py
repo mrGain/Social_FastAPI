d557cab102044384ff41cf8c6fec00de66bc5fdd
Rakesh Gain <rakeshgain47681@gmail.com>
2023-09-03 22:00:54 +0530

    models definded

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from .database import Base
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(50), nullable=False)
+    content = Column(String(255), nullable=False)
+    published = Column(Boolean, nullable=False, default=True) 
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    # created_at = Column(DateTime, server_default=text('now()'))
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    email = Column(String(100), nullable=False, unique=True)
+    password = Column(String(300), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
