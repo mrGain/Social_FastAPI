f73bc46a1be2c94ff2970f6f1a1155a5bd53ca8c
Rakesh Gain <rakeshgain47681@gmail.com>
2023-09-08 12:26:18 +0530

    wired the votes router

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from typing import Optional
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
 from .database import engine, get_db
-from .routers import post, user, auth
+from .routers import post, user, auth, vote
 # from .config import settings
 
 models.Base.metadata.create_all(bind=engine)
@@ -13,6 +13,7 @@ app = FastAPI()
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
 
 @app.get("/")
 async def root():
