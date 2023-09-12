f88ea54af778854299db0abbf5fbe5c4734dc70f
Rakesh Gain <rakeshgain47681@gmail.com>
2023-09-12 14:36:02 +0530

    handeled the post not found exception.

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -10,6 +10,14 @@ router = APIRouter(
 
 @router.post("/votes", status_code=status.HTTP_201_CREATED)
 async def create_vote(vote: schemas.Vote, db: Session = Depends(get_db), current_user=Depends(oauth2.get_current_user)):
+    
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post doesn't exist."
+        )
+    
     vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id)
     found_vote = vote_query.first()
 
