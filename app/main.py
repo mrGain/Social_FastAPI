a09fd1e6330ebc4d704b6853b38cfb8044628608
Rakesh Gain <rakeshgain47681@gmail.com>
2023-09-19 22:52:40 +0530

    cores middleware is added

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, status, Response, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
@@ -6,9 +7,21 @@ from .database import engine, get_db
 from .routers import post, user, auth, vote
 # from .config import settings
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
     
 app = FastAPI()
+
+origins = [
+    "https://www.google.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
